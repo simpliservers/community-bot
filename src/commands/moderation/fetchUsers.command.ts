59705b89b93dc2b42d65731485f1d7b2f5bc27c4
Aiken Tine Ahac <ahac.aiken@gmail.com>
2022-01-17 15:40:20 +0100

    Attempted creation fix v4

--- src/commands/moderation/fetchUsers.command.ts
+++ src/commands/moderation/fetchUsers.command.ts
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import Member from '../../api/api';
 import { Logger } from 'tslog';
+import { checkForUser } from '../../utils';
+import { Interaction } from 'discord.js';
 
 const log = new Logger();
 
@@ -8,14 +9,31 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('fetch-members')
     .setDescription('Fetches all users from the server.'),
-  async execute(interaction: any) {
+  async execute(interaction: Interaction) {
+    if (!interaction.isCommand()) return;
+    if (!interaction.guild)
+      return interaction.reply({
+        content: 'No guild found.',
+        ephemeral: true,
+      });
+    if (!interaction.channel)
+      return interaction.reply({
+        content: 'No channel found.',
+        ephemeral: true,
+      });
+    if (interaction.channel.type != 'GUILD_TEXT')
+      return interaction.reply({
+        content: "You can't do this here.",
+        ephemeral: true,
+      });
+
     const member = await interaction.guild.members.fetch(interaction.user.id);
 
-    if (member.permissions.has('ADMIN')) {
+    if (member.permissions.has('ADMINISTRATOR')) {
       const members: any = await interaction.guild.members.fetch();
       members.forEach(async (member: any) => {
         log.info(`${member.user.tag} - ${member.user.id}`);
-        await Member.create(member.user.tag, member.user.id);
+        await checkForUser(member.user.id);
       });
 
       interaction.reply(`Fetched all users!`);
